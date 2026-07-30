4114bdd185774d147f8e4fa5a09c94d763df0a23
Brian Langseth <brian.langseth@noaa.gov>
2026-07-30 14:42:18 -0700

    Add bds data to getArea() #17

--- R/getArea.R
+++ R/getArea.R
@@ -11,11 +11,18 @@
 #' @section Area filtering rules:
 #' Values in `source` are evaluated using case-insensitive matching.
 #'
-#' For recent RecFIN data (`source = "RECFIN_WATER_AREA_NAME"`), records are
-#' removed when area is:
+#' For recent RecFIN catch data (`source = "RECFIN_WATER_AREA_NAME"`), records 
+#' are removed when area is:
 #' * `CANADA`
 #' * `MEXICO`
 #' * `PUGET SOUND`
+#' 
+#' For recent RecFIN bds data (`source = "AGENCY_FISHED_AREA_NAME"`), records
+#' are kept when area is 
+#' * For Washington: PUNCH CARD AREAs 1 through 4, and PUNCH CARD AREAs 0 and 
+#' 'Not Known' when RECFIN_PORT_NAME also equals coastal ports.
+#' * For California: Because AGENCY_FISHED_AREA_NAME is empty for California,
+#' the script automatically uses "AGENCY_WATER_AREA_NAME" for california data. 
 #'
 #' For Washington historical data (`source = "AREA"` and `AGENCY == "W"`),
 #' records with `AREA >= 5` are removed.
@@ -37,6 +44,11 @@
 #' of Canada, Mexico, and Puget Sound. Areas with `Not Known` are kept.
 #' For Washington historical catch data, uses `AREA`, which filters out values
 #' of 5 and greater (i.e. Puget Sound)
+#' For recent bds data, use `AGENCY_FISHED_AREA_NAME`, which filters out values 
+#' of Canada, Mexico, and Puget Sound. Areas with "Not known" or "Unknown" 
+#' are kept if they also have coastal port names. Because California data are
+#' empty for `AGENCY_FISH_AREA_NAME`, the code instead filters California data 
+#' using "AGENCY_WATER_AREA_NAME" when `AGENCY_FISHED_AREA_NAME` is used.
 #' For all other data sets, use any valid column, since for these areas no
 #' specific records outside federal waters are identifiable.
 #'
@@ -55,7 +67,9 @@ getArea <- function(
 
   flag <- FALSE
 
-  if (source == "RECFIN_WATER_AREA_NAME") { # Recent catch data
+  
+  ## Recent catch data
+  if (source == "RECFIN_WATER_AREA_NAME") { 
 
     nonfed <- c(
       "CANADA",
@@ -88,8 +102,10 @@ getArea <- function(
     flag <- TRUE
   }
 
+  
+  ## Washington historical catch data
   if (source == "AREA" &
-    all(data$AGENCY == "W") & # Washington historical catch data
+    all(data$AGENCY == "W") & 
     length(data$AGENCY > 0)) { # Ensure AGENCY exists (if not 'all' returns TRUE)
 
     removed <- data |>
@@ -114,6 +130,58 @@ getArea <- function(
       ))
     }
 
+    flag <- TRUE
+  }
+  
+  
+  ## Recent bds data
+  if (source %in% c("AGENCY_FISHED_AREA_NAME")) { 
+    
+    wa_fed <- c(
+      "PUNCH CARD AREA 0",
+      "PUNCH CARD AREA 1",
+      "PUNCH CARD AREA 2",
+      "PUNCH CARD AREA 3",
+      "PUNCH CARD AREA 4",
+      "NOT KNOWN"
+    )
+
+    removed <- data |>
+      dplyr::filter(dplyr::case_when(
+        STATE_NAME == "WASHINGTON" & .data[[source]] %in% "NOT KNOWN" ~ 
+          !RECFIN_PORT_NAME %in% c("LA PUSH", "NEAH BAY", "SEKIU", "WESTPORT"),
+        STATE_NAME == "WASHINGTON" ~ !tolower(.data[[source]]) %in% tolower(wa_fed),
+        STATE_NAME == "CALIFORNIA" ~ grepl("MEXICO", .data$AGENCY_WATER_AREA_NAME))
+        )
+    
+    data <- data |>
+      dplyr::filter(dplyr::case_when(
+        STATE_NAME == "WASHINGTON" & .data[[source]] %in% "NOT KNOWN" ~ 
+          RECFIN_PORT_NAME %in% c("LA PUSH", "NEAH BAY", "SEKIU", "WESTPORT"),
+        STATE_NAME == "WASHINGTON" ~ tolower(.data[[source]]) %in% tolower(fed),
+        STATE_NAME == "CALIFORNIA" ~ !grepl("MEXICO", .data$AGENCY_WATER_AREA_NAME))
+      )
+    
+    noarea <- nrow(removed)
+    ncan <- sum(removed[, source] == "PUNCH CARD AREA 20", na.rm = TRUE)
+    nsound <- sum(grepl("PUNCH CARD AREA", removed[, source]), na.rm = TRUE) - ncan
+    nmex <- sum(grepl("MEXICO", removed[, "AGENCY_WATER_AREA_NAME"]), na.rm = TRUE)
+    nunk <- sum(removed[, source] %in% c("NOT KNOWN", "UNKNOWN"), na.rm = TRUE) +
+      sum(is.na(removed[, source]), na.rm = TRUE)
+    
+    if (verbose) {
+      cli::cli_bullets(c(
+        " " = "{.fn getArea} summary information -",
+        "i" = "There are {noarea} records outside federal waters and were removed.",
+        "i" = "These include {ncan} records from Canada",
+        "i" = "These include {nsound} records from Puget Sound",
+        "i" = "These include {nmex} records from Mexico",
+        "i" = "There are {nunk} records designated as Not Known or Unknown that 
+        could also not be associated with federal areas in other fields and so 
+        were removed."
+      ))
+    }
+    
     flag <- TRUE
   }
 
