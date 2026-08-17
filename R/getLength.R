a58fe4513ebef78e51c40fdb424c7c7867b6e105
Brian Langseth <brian.langseth@noaa.gov>
2026-08-17 14:27:46 -0700

    Update flags for MRFSS lengths
    #Closes #35

--- R/getLength.R
+++ R/getLength.R
@@ -3,7 +3,8 @@
 #' @details
 #' This function is used for only composition data. It creates a new column
 #' in units of cm, and also flags lengths beyond max, as well as total length
-#' measurements (as opposed to fork length). Removes lengths with NA or 0
+#' measurements (as opposed to fork length) or of measurements not directly 
+#' measured. Removes lengths with NA or 0
 #'
 #' @section Length extraction rules:
 #' `source` can be a vector of candidate column names. The first matching
@@ -12,7 +13,9 @@
 #' The selected source column is copied directly into a standardized `length_cm`
 #' column. The units are cm and adjusted based on whether the input source
 #' column includes mm or cm. Extreme length values are flagged but not removed,
-#' as are measurements in Total length. Lengths that are NA or 0 are removed.
+#' as are measurements in total length for recent data. MRFSS data have separate
+#' columns for fork length (LNGTH) or total length (T_LEN). Lengths that are NA 
+#' or 0 are removed.
 #'
 #' If `verbose = TRUE`, the function reports how many records with unknown or 0
 #' length were removed, alongwith a message conveying the number of extreme
@@ -24,10 +27,10 @@
 #' @inheritParams clean_bds
 #'
 #' @param source Column name where length information is located. Depends on the
-#' era (recent, mrfss, or historical). Default value is for recent catch data
-#' (RECFIN_LENGTH_MM). Coded to accept a vector of names where the same type or
-#' era of data has multiple different names. When multiple names within the
-#' vector are in the dataset, picks the first.
+#' era (recent, mrfss). Default value is for recent catch data
+#' (RECFIN_LENGTH_MM). Coded to accept a vector of names where the same era of 
+#' data has multiple different names. When multiple names within the vector are 
+#' in the dataset, picks the first.
 #'
 
 getLength <- function(
@@ -89,14 +92,30 @@ getLength <- function(
     # Note that LENFLAG only occurs in 2004 (for CRFS samples)
     nolen <- nrow(removed)
     
+    if(source == "LNGTH") {
+      #ltype = "fork length" #source length type
+      other_l = "T_LEN" #the other length measurement
+      #other_ltype = "total length" #the other length measurement type
+    }
+    if(source == "T_LEN") {
+      #ltype = "total length" #source length type
+      other_l = "LNGTH" #the other length measurement
+      #other_ltype = "fork length" #the other length measurement
+    }
+    
     if (verbose) {
       cli::cli_bullets(c(
         " " = "{.fn getLength} summary information -",
         "i" = "There are {nolen} records where length was NA or 0 and were removed",
-        "i" = "NOTE: There are records where {source} was derived from empirical
-        relationships based on other types of measurements, likely another length
-        measurement but possibly a weight. The user should decide how to handle 
-        these"
+        "i" = "NOTE: There are records where {source} was likely derived from 
+        empirical relationships from another measurement, either from {other_l} or
+        WGT. This can be inferred based on the number of decimal places. 
+        The user should decide how to handle these by checking the number of
+        decimals of {source} compared to {other_l} or WGT. For applications 
+        where precision is important, such as for W-L or growth relationships, 
+        only direct measured samples are encouraged. For compositions, where
+        imprecision in the empirical relationship is likely to be less than the 
+        bin size used, measurements obtained from conversions are likely suitable."
       ))
     }
   }
